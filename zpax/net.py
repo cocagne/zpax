21a7154bb18935065bac57799a20b99895aa3393
Tom Cocagne <tom.cocagne@gmail.com>
2012-12-11 16:52:05 -0600

    switched to router socket

--- zpax/net.py
+++ zpax/net.py
@@ -18,9 +18,9 @@ class NetworkNode (object):
 
         self.node_uid         = node_uid
 
-        self.zpax_nodes       = None # Dictionary of node_uid -> (rep_addr, pub_addr)
+        self.zpax_nodes       = None # Dictionary of node_uid -> (rtr_addr, pub_addr)
 
-        self.pax_rep          = None
+        self.pax_rtr          = None
         self.pax_pub          = None
         self.pax_sub          = None
         self.dispatch_message = lambda x: None
@@ -28,43 +28,50 @@ class NetworkNode (object):
 
     def connect(self, zpax_nodes):
         '''
-        zpax_nodes - Dictionary of node_uid => (zmq_rep_addr, zmq_pub_addr)
+        zpax_nodes - Dictionary of node_uid => (zmq_rtr_addr, zmq_pub_addr)
         '''
         if not self.node_uid in zpax_nodes:
             raise Exception('Missing local node configuration')
 
         self.zpax_nodes = zpax_nodes
         
-        if self.pax_rep:
-            self.pax_rep.close()
+        if self.pax_rtr:
+            self.pax_rtr.close()
             self.pax_pub.close()
             self.pax_sub.close()
 
-        self.pax_rep = tzmq.ZmqRepSocket()
+        self.pax_rtr = tzmq.ZmqRouterSocket()
         self.pax_pub = tzmq.ZmqPubSocket()
-        self.pax_sub = tzmq.ZmqSubSocket()        
+        self.pax_sub = tzmq.ZmqSubSocket()
+
+        self.pax_rtr.identity = self.node_uid
                     
-        self.pax_rep.linger = 0
+        self.pax_rtr.linger = 0
         self.pax_pub.linger = 0
         self.pax_sub.linger = 0
         
-        self.pax_rep.bind(zpax_nodes[self.node_uid][0])
+        self.pax_rtr.bind(zpax_nodes[self.node_uid][0])
         self.pax_pub.bind(zpax_nodes[self.node_uid][1])
 
-        self.pax_rep.messageReceived = self._on_rep_received
+        self.pax_rtr.messageReceived = self._on_rtr_received
         self.pax_sub.messageReceived = self._on_sub_received
 
         self.pax_sub.subscribe = 'zpax'
         
-        for x in cur_remote:
-            self.pax_sub.connect(x)
+        for node_uid, tpl in zpax_nodes.iteritems():
+            self.pax_sub.connect(tpl[1])
+            if self.node_uid < node_uid:
+                # We only need 1 connection between any two router nodes so
+                # we'll make it the responsibility of the lower UID node to
+                # initiate the connection
+                self.pax_rtr.connect(tpl[0])
 
 
     def shutdown(self):
-        self.pax_rep.close()
+        self.pax_rtr.close()
         self.pax_pub.close()
         self.pax_sub.close()
-        self.pax_rep = None
+        self.pax_rtr = None
         self.pax_pub = None
         self.pax_sub = None
 
@@ -79,7 +86,7 @@ class NetworkNode (object):
         # TODO
 
 
-    def _on_rep_received(self, parts):
+    def _on_rtr_received(self, parts):
         self.dispatch_message( parts )
 
 
