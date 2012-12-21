1d63938e39a122ca942027872c71eb361a49e51f
Tom Cocagne <tcocagne@cleversafe.com>
2012-12-21 16:59:32 -0600

    working on updating keyval module

--- zpax/keyval2.py
+++ zpax/keyval2.py
@@ -1,6 +1,3 @@
-# Switch net node to use a dictionary of "message_type" => dispatch function obj
-
-
 '''
 This module implements a simple, distributed Key-Value database that uses Paxos
 for ensuring consistency between nodes. The goal of this module is to provide a
@@ -21,18 +18,13 @@ from twisted.internet import defer, task, reactor
 
 _ZPAX_CONFIG_KEY = '__zpax_config__'
 
-
 tables = dict()
 
 tables['kv'] = '''
 key      text PRIMARY KEY,
 value    text,
-resolution integer
+instance integer
 '''
-
-
-class MissingConfiguration (Exception):
-    pass
 
 
 class SqliteDB (object):
@@ -54,7 +46,7 @@ class SqliteDB (object):
         for k,v in tables.iteritems():
             cur.execute('create table {} ({})'.format(k,v))
 
-        cur.execute('create index resolution_index on kv (resolution)')
+        cur.execute('create index instance_index on kv (instance)')
 
         self._con.commit()
         cur.close()
@@ -66,36 +58,36 @@ class SqliteDB (object):
             return r[0]
 
         
-    def get_resolution(self, key):
-        r = self._cur.execute('SELECT resolution FROM kv WHERE key=?', (key,)).fetchone()
+    def get_instance(self, key):
+        r = self._cur.execute('SELECT instance FROM kv WHERE key=?', (key,)).fetchone()
         if r:
             return r[0]
 
         
-    def update_key(self, key, value, resolution_number):
-        prevpn = self.get_resolution(key)
+    def update_key(self, key, value, instance_number):
+        prevpn = self.get_instance(key)
 
         if prevpn is None:
             self._cur.execute('INSERT INTO kv VALUES (?, ?, ?)',
-                              (key, value, resolution_number))
+                              (key, value, instance_number))
             self._con.commit()
             
-        elif resolution_number > prevpn:
-            self._cur.execute('UPDATE kv SET value=?, resolution=? WHERE key=?',
-                              (value, resolution_number, key))
+        elif instance_number > prevpn:
+            self._cur.execute('UPDATE kv SET value=?, instance=? WHERE key=?',
+                              (value, instance_number, key))
             self._con.commit()
 
             
-    def get_last_resolution(self):
-        r = self._cur.execute('SELECT MAX(resolution) FROM kv').fetchone()[0]
+    def get_last_instance(self):
+        r = self._cur.execute('SELECT MAX(instance) FROM kv').fetchone()[0]
 
         return r if r is not None else -1
 
     
-    def iter_updates(self, start_resolution, end_resolution=2**32):
+    def iter_updates(self, start_instance, end_instance=2**32):
         c = self._con.cursor()
-        c.execute('SELECT key,value,resolution FROM kv WHERE resolution>? AND resolution<?  ORDER BY resolution',
-                  (start_resolution, end_resolution))
+        c.execute('SELECT key,value,instance FROM kv WHERE instance>? AND instance<?  ORDER BY instance',
+                  (start_instance, end_instance))
         return c
 
 
@@ -123,16 +115,16 @@ class KeyValNode (multi.MultiPaxosHeartbeatNode):
         self.enabled = kw['instance'] == self.kvdb.last_instance + 1
 
         if not self.enabled:
-            self.kvdb.catchup()
+            self.kvdb.catchup(kw['instance'])
         
 
     def on_resolution(self, proposer_obj, proposal_id, value):
         # This method is only called when our database is current
         assert self.instance == self.kvdb.last_instance + 1
         
-        key, value = json.loads(value[0])
+        key, value = json.loads(value[1])
 
-        self.kvdb.onValueSet( key, value, instance_num )
+        self.kvdb.on_paxos_resolution( key, value, self.instance )
 
         super(KeyValNode,self).on_resolution(proposer_obj, proposal_id, value)
         
@@ -179,231 +171,173 @@ class KeyValueDB (object):
     simplicity.
     '''
 
-    _node_klass = KeyValNode
+    catchup_retry_delay = 2.0
+    catchup_num_items   = 2
     
-    def __init__(self, node_uid,
+    def __init__(self, net_node, quorum_size,
                  database_dir,
-                 database_filename=None,
-                 catchup_retry_delay=2.0,
-                 catchup_num_items=2):
+                 database_filename=None):
 
         if database_filename is None:
             database_filename = os.path.join(database_dir, 'db.sqlite')
-
-        if database_filename == ':memory:':
-            durable_dir = None
-            durable_id  = None
-        else:
-            durable_dir = database_dir
-            durable_id  = os.path.basename(database_filename + '.paxos')
-
-        self.node_uid = node_uid
-        self.kv_node = self._node_klass(self, node_uid, durable_dir, durable_id)
 
         # By default, prevent arbitrary clients from proposing new
         # configuration values
         self.allow_config_proposals = False
 
-        self.catchup_retry_delay = catchup_retry_delay
-        self.catchup_num_items   = catchup_num_items
-            
         self.db            = SqliteDB( database_filename )
-        self.db_seq        = self.db.get_last_resolution()
+        self.last_instance = self.db.get_last_instance()
         self.catching_up   = False
         self.catchup_retry = None
+        self.active_instance = None
 
-        self.rep_addr      = None
-        self.other_reps    = None
-            
-        self.rep           = None
-        self.dlr           = None
+        self.kv_node = KeyValNode(self, net_node, quorum_size)
+        self.net     = net_node
 
-        if self.isInitialized():
-            self._loadConfiguration()
+        self.net.message_handler.append(self)
+
+        if self.initialized:
+            self._load_configuration()
 
 
-    def onCaughtUp(self):
-        pass
+    def _load_configuration(self):
 
-    
-    def _loadConfiguration(self, cfg_str=None):
-        if cfg_str is None:
-            cfg_str = self.db.get_value(_ZPAX_CONFIG_KEY)
+        self.last_instance = self.db.get_last_instance()
 
-        cfg = json.loads(cfg_str)
+        cfg = json.loads( self.db.get_value(_ZPAX_CONFIG_KEY) )
 
         zpax_nodes = dict()
-        kv_reps    = set()
-        my_addr    = None
+        all_nodes  = set()
 
         for n in cfg['nodes']:
-            zpax_nodes[ n['uid'] ] = (n['pax_rep_addr'], n['pax_pub_addr'])
+            zpax_nodes[ n['uid'] ] = (n['pax_rtr_addr'], n['pax_pub_addr'])
+            all_nodes.add( n['uid'] )
             
-            if self.kv_node.node_uid == n['uid']:
-                my_addr = n['kv_rep_addr']
-            else:
-                kv_reps.add( n['kv_rep_addr'] )
+        quorum_size = len(cfg['nodes'])/2 + 1
 
-        if my_addr is None:
-            raise MissingConfiguration('Configuration is missing configuration for this node')
+        if self.kv_node.quorum_size != quorum_size:
+            self.kv_node.change_quorum_size( quorum_size )
 
-        if self.rep_addr is None or self.rep_addr != my_addr:
-            self.rep_addr = my_addr
-            if self.rep is not None:
-                self.rep.close()
-            self.rep = tzmq.ZmqRepSocket()
-            self.rep.linger = 0
-            self.rep.bind(self.rep_addr)
-            self.rep.messageReceived = self._generateResponder( '_REP_' )
+        if not self.net.node_uid in all_nodes:
+            # We've been removed from the inner circle
+            print 'This node has been removed from the Paxos group'
+            self.shutdown()
 
-        if self.other_reps is None or not self.other_reps == kv_reps:
-            self.other_reps = kv_reps
-            if self.dlr is not None:
-                self.dlr.close()
-            self.dlr = tzmq.ZmqDealerSocket()
-            self.dlr.messageReceived = self._generateResponder( '_DLR_',
-                                                                lambda x : x[1:])
-
-            for x in kv_reps:
-                self.dlr.connect(x)
-                
-
-        # Quorum size may be specified in the config if tighter consistency
-        # requirements are desired
-        if 'quorum_size' in cfg:
-            quorum_size = cfg['quorum_size']
-        else:
-            quorum_size = len(cfg['nodes'])/2 + 1
-
-        if not self.kv_node.initialized:
-            self.kv_node.initialize( quorum_size )
-
-        elif self.kv_node.quorum_size != quorum_size:
-            self.kv_node.changeQuorumSize( quorum_size )
-
-        self.kv_node.connect( zpax_nodes )
+        self.net_node.connect( zpax_nodes )
 
 
-    def isInitialized(self):
+    @property
+    def initialized(self):
         return self.db.get_value(_ZPAX_CONFIG_KEY) is not None
 
     
     def initialize(self, config_str):
-        if self.isInitialized():
+        if self.initialized:
             raise Exception('Node already initialized')
         
         self.db.update_key(_ZPAX_CONFIG_KEY, config_str, -1)
-        self._loadConfiguration()
+        self._load_configuration()
 
                 
     def shutdown(self):
-        if self.dlr:
-            self.dlr.close()
-        self.rep.close()
         if self.catchup_retry and self.catchup_retry.active():
             self.catchup_retry.cancel()
         self.kv_node.shutdown()
 
+            
+    def on_paxos_resolution(self, key, value, instance_num):
+        assert not self.catching_up
         
-    def getMaxDBSequenceNumber(self):
-        return self.db_seq
+        self.db.update_key( key, value, instance_num )        
+        self.last_instance = instance_num
+        
+        if key == _ZPAX_CONFIG_KEY:
+            self._load_configuration()
+        
+
+    def catchup(self, active_instance):
+        self.active_instance = active_instance
+        
+        if self.catching_up:
+            return 
+
+        self.catching_up = True
+
+        self._catchup()
+        
+
+    def _catchup(self):
+        if self.catching_up:
+            self.catchup_retry = reactor.callLater(self.catchup_retry_delay,
+                                                   self._catchup)
+
+            if self.kv_node.leader_uid is not None:
+                self.unicast( self.kv_node.leader_uid,
+                              'catchup_request',
+                              last_known_instance=self.last_instance ) )
+        
+
+    #--------------------------------------------------------------------------
+    # Messaging
+    #           
+    def unicast(self, to_uid, message_type, **kwargs):
+        self.net.unicast( to_uid, message_type, json.dumps(kwargs) )
+
+
+    def receive_catchup_request(self, from_uid, msg):
+        l = list()
+        
+        for tpl in self.db.iter_updates(msg['last_known_instance']):
+            l.append( tpl )
+            if len(l) == self.catchup_num_items:
+                break
+
+        self.unicast( from_uid, 'catchup_data',
+                      from_instance = header['last_known_instance'],
+                      key_val_instance_list = l )
 
     
-    def isCatchingUp(self):
-        return self.catching_up
-
-
-    def onValueSet(self, key, value, instance_num):
-        if key == _ZPAX_CONFIG_KEY:
-            try:
-                self._loadConfiguration( value )
-            except MissingConfiguration:
-                pass # We've been removed from the inner circle :(
-        self.db.update_key( key, value, instance_num )        
-        self.db_seq = instance_num
-
-
-    def catchup(self):
-        if self.catching_up or self.db_seq == self.kv_node.getCurrentSequenceNumber() - 1:
-            return 
-        
-        self._catchup()
-
-        
-    def _catchup(self):
-        self.catching_up = self.db_seq != self.kv_node.getCurrentSequenceNumber() - 1
-        
-        if not self.catching_up:
-            self.onCaughtUp()
-            return
-
-        self.catchup_retry = reactor.callLater(self.catchup_retry_delay,
-                                               self._catchup)
-
-        self.dlr.send( '', json.dumps( dict(type='catchup_request', last_known_seq=self.db_seq) ) )
-        
-
-        
-    #--------------------------------------------------------------------------
-    # REQ Socket
-    #
-    def _DLR_catchup_data(self, msg):
-        if self.db_seq != msg['from_seq']:
-            # Reply to an old request. Ignore it.
+    def receive_catchup_data(self, from_instance):
+        if self.last_instance != from_instance:
+            # This is a reply to an old request. Ignore it.
             return
             
         if self.catchup_retry and self.catchup_retry.active():
             self.catchup_retry.cancel()
             self.catchup_retry = None
+
+        reload_config = False
+        
+        for key, val, instance_num in msg['key_val_instance_list']:
+            self.db.update_key(key, val, instance_num)
             
-        if msg['from_seq'] == self.db_seq:
-            for key, val, seq_num in msg['key_val_seq_list']:
-                if key == _ZPAX_CONFIG_KEY:
-                    try:
-                        self._loadConfiguration(val)
-                    except MissingConfiguration:
-                        # We've been removed from the inner circle :(
-                        pass
-                self.db.update_key(key, val, seq_num)
+            if key == _ZPAX_CONFIG_KEY:
+                reload_config = True
+
+        if reload_config:
+            self._load_configuration()
                 
-            self.db_seq = self.db.get_last_resolution()
-
-        self._catchup()
-
-    #--------------------------------------------------------------------------
-    # REP Socket
-    #           
-    def rep_reply(self, **kwargs):
-        self.rep.send( json.dumps(kwargs) )
-
+        self.last_instance = self.db.get_last_instance()
         
-    def _REP_propose_value(self, header):
+        self.catching_up = self.active_instance != self.last_instance
+
+        if self.catching_up:
+            self._catchup()
+
+    
+    def receive_propose_value(self, from_uid, msg):
         try:
-            if not self.allow_config_proposals and header['key'] == _ZPAX_CONFIG_KEY:
+            if not self.allow_config_proposals and msg['key'] == _ZPAX_CONFIG_KEY:
                 raise Exception('Access Denied')
-            jstr = json.dumps( [header['key'], header['value']] )
-            self.kv_node.proposeValue(jstr)
-            self.rep_reply( proposed = True )
+            self.kv_node.proposeValue( msg['request_id'], [msg['key'], msg['value']] ) )
+            self.unicast(from_uid, 'propose_reply', request_id=msg['request_id'])
         except node.ProposalFailed, e:
-            self.rep_reply(proposed=False, message=str(e))
+            self.unicast(from_uid, 'propose_reply', request_id=msg['request_id'], error=str(e))
 
             
-    def _REP_query_value(self, header):
-        if not self.allow_config_proposals and header['key'] == _ZPAX_CONFIG_KEY:
-            self.rep_reply(error='Access Denied')
+    def receive_query_value(self, from_uid, msg):
+        if not self.allow_config_proposals and msg['key'] == _ZPAX_CONFIG_KEY:
+            self.unicast(from_uid, 'query_result', error='Access Denied')
         else:
-            self.rep_reply( value = self.db.get_value(header['key']) )
-
-
-    def _REP_catchup_request(self, header):
-        l = list()
-        
-        for tpl in self.db.iter_updates(header['last_known_seq']):
-            l.append( tpl )
-            if len(l) == self.catchup_num_items:
-                break
-
-        self.rep_reply( type             = 'catchup_data',
-                        from_seq         = header['last_known_seq'],
-                        key_val_seq_list = l )
+            self.unicast(from_uid, 'query_result', value=self.db.get_value(msg['key']) )
 
