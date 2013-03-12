159e03092806a6115f241016bd0df2fbf2f44595
tcocagne <tcocagne@cleversafe.com>
2013-03-12 07:13:22 -0500

    Added network channel class

--- zpax/net.py
+++ zpax/net.py
@@ -12,6 +12,37 @@ class SimpleEncoder(object):
         return from_uid, message_type, parts[1:]
     
 
+class Channel (object):
+    '''
+    Wraps a NetworkNode object with an interface that sends and receives over
+    a specific channel
+    '''
+
+    def __init__(self, channel_name, net_node):
+        self.channel_name = channel_name
+        self.net_node     = net_node
+
+        
+    def add_message_handler(self, handler):
+        self.net_node.add_message_handler(self.channel_name, handler)
+        
+
+    def connect(self, *args, **kwargs):
+        self.net_node.connect(*args, **kwargs)
+
+
+    def shutdown(self):
+        self.net_node.shutdown()
+
+
+    def broadcast(self, message_type, *parts):
+        self.net_node.broadcast_message(self.channel_name, message_type, *parts)
+
+
+    def unicast(self, to_uid, message_type, *parts):
+        self.net_node.unicast_message(to_uid, self.channel_name, message_type, *parts)
+
+        
 class NetworkNode (object):
     '''
     Messages are handled by adding instances to the message_handlers list. The
